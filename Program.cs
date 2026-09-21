7c05c134e76bb12ce21ff0288acb6d317b8159dd
Rathi2005 <h0rshitr0thi2005@gmail.com>
2026-09-22 00:20:25 +0530

    Remove database credentials

--- Program.cs
+++ Program.cs
@@ -97,14 +97,14 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
 
-}
+// }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
